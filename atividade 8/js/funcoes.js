ac634d3c71721834e51a165ec3ad5b93bcc0bf1c
daniel <danielkira01@gmail.com>
2026-06-23 21:47:45 -0300

    adicionando atividade 8

---
+++ atividade 8/js/funcoes.js
@@ -0,0 +1,36 @@
+function soma() {
+    let primeiroNumero = document.getElementById("primeiro-numero").value;
+    let segundoNumero = document.getElementById("segundo-numero").value;
+
+    let resultado = Number(primeiroNumero) + Number(segundoNumero);
+
+    document.getElementById("resultado").innerText = resultado;
+
+    salvarHistorico("Soma",primeiroNumero, segundoNumero, resultado)
+}
+function subtrair() {
+    let primeiroNumero = document.getElementById("primeiro-numero").value;
+    let segundoNumero = document.getElementById("segundo-numero").value;
+
+    let resultado = Number(primeiroNumero) + Number(segundoNumero);
+
+    document.getElementById("resultado").innerText = resultado;
+
+    salvarHistorico("Soma",primeiroNumero, segundoNumero, resultado)
+}
+function dividir() {
+    let primeiroNumero = document.getElementById("primeiro-numero").value;
+    let segundoNumero = document.getElementById("segundo-numero").value;
+
+    let resultado = Number(primeiroNumero) + Number(segundoNumero);
+
+    document.getElementById("resultado").innerText = resultado;
+
+    salvarHistorico("dividir",primeiroNumero, segundoNumero, resultado)
+}
+function salvarHistorico(nomeDaFuncao, num1, num2, resultado) {
+    console.log("Função: " + nomeDaFuncao);
+    console.log("Primeiro numero: " + num1 + ", SegundoNumero: " + num2);
+    console.log("Resultado: " + resultado);
+    console.log("--------------------------------------");
+}
